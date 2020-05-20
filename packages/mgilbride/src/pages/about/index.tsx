305a972d03c21ddc542385eb16cf83eba79e73cd
matthewtgilbride <matthewtgilbride@gmail.com>
2020-05-19 20:51:59 -0400

    feat(mgilbride): copy or about and resume
    Finish about page, initial copy for resume

--- packages/mgilbride/src/pages/about/index.tsx
+++ packages/mgilbride/src/pages/about/index.tsx
@@ -42,15 +42,19 @@ const Home: FC = () => (
           out my&nbsp;<Link to="/resume">resume</Link> for more about that.
         </p>
         <p>
-          This page serves as a place for people to find out more about me, and
+          This site serves as a place for people to find out more about me, and
           a place for <Link to="/blog">me to write down my thoughts</Link>. Most
           of those thoughts will be about programming for now, so I&apos;ll keep
           this particular page focused on everything else.
         </p>
         <p>
-          I was born and raised here, though my mom&apos;s family is from
-          Barcelona. Many moons ago I was fluent in Spanish. I do my best to
-          maintain my skills in that language as well. I prefer travelling to
+          I&apos;m a happily married man to my wife Jamie of (almost) four
+          years. We have zero kids and one dog named Emmy (for now).
+        </p>
+        <p>
+          I was born and raised here in Philly, though my mom&apos;s family is
+          from Barcelona. Many moons ago I was fluent in Spanish. I do my best
+          to maintain my skills in that language as well. I prefer travelling to
           Spanish speaking countries not only because I love the culture, but
           also because I love the practice. That&apos;s right, we&apos;re
           talkin&apos; &apos;bout practice.
@@ -60,6 +64,17 @@ const Home: FC = () => (
           bigger fan of the Philadelphia 76ers. I follow all Philly sports
           closely, but I&apos;d say the passion level goes Sixers, Eagles,
           Flyers, Phillies, in that order. #trusttheprocess
+        </p>
+        <p>
+          I also enjoy playing tennis, hiking, biking, running, skiing, and
+          snowboarding, amongst other things. I&apos;m not much of a cook, but
+          like most I&apos;m dangerous with a handful of recipes. On the music
+          front, my bag is mostly blues and rock but I&apos;ve got a pretty
+          eclectic taste.
+        </p>
+        <p>
+          Oh...and my top 3 movies of all time are Forrest Gump, The Shawshank
+          Redemption, and The Big Lebowski.
         </p>
         <p>
           That&apos;s all for now. If you are interested in connecting for any
--- packages/mgilbride/src/pages/resume/index.tsx
+++ packages/mgilbride/src/pages/resume/index.tsx
@@ -1,3 +1,93 @@
-import UnderConstruction from '../../components/UnderConstruction';
+import React, { FC } from 'react';
+import { CSSObject } from '@emotion/core';
+import { Layout } from '../../components/Layout';
+import { makeSpace, responsiveBreakpoints } from '../../utils/design';
 
-export default UnderConstruction; // eslint-disable-line import/no-default-export
+const styleContainer: CSSObject = {
+  display: 'grid',
+  justifySelf: 'center',
+  justifyItems: 'center',
+  padding: makeSpace('md'),
+  maxWidth: responsiveBreakpoints.tabletPortrait,
+  img: {
+    height: '50vh',
+    paddingBottom: makeSpace('sm'),
+  },
+  'h1, h2': {
+    fontStyle: 'italic',
+    margin: `${makeSpace('sm')} 0`,
+  },
+  '.first': {
+    marginTop: 0,
+  },
+  h4: {
+    margin: `${makeSpace('xs')} 0 0 0`,
+  },
+  ul: {
+    padding: `0 ${makeSpace('sm')}`,
+    margin: 0,
+  },
+  li: {
+    margin: `${makeSpace('xxs')} 0`,
+    listStyleType: 'circle',
+  },
+  a: {
+    textDecoration: 'underline',
+  },
+};
+
+const Home: FC = () => (
+  <Layout>
+    <div css={styleContainer}>
+      <div>
+        <h1 className="first">Skills</h1>
+        <h2>Languages / Frameworks</h2>
+        <h4>Day to Day</h4>
+        <ul>
+          <li>Javascript, Typescript, Node</li>
+          <li>React, Vue, HTML, CSS</li>
+          <li>Scala, Akka, Python, Flask</li>
+          <li>SQL, Postgres</li>
+        </ul>
+        <h4>Previous Projects</h4>
+        <ul>
+          <li>Java, Spring</li>
+          <li>Neo4j, Apache Spark</li>
+        </ul>
+        <h4>Dabbled</h4>
+        <ul>
+          <li>Clojure, Haskell</li>
+        </ul>
+        <h2>Architecture</h2>
+        <ul>
+          <li>REST</li>
+          <li>Event Sourcing and CQRS</li>
+          <li>Domain Driven Design</li>
+        </ul>
+        <h2>Infrastructure</h2>
+        <ul>
+          <li>Git, GitHub, GitLab</li>
+          <li>Docker</li>
+          <li>Jenkins</li>
+        </ul>
+        <h4>AWS</h4>
+        <ul>
+          <li>EC2, ECS</li>
+          <li>Lambda</li>
+          <li>Cognito</li>
+        </ul>
+        <h2>Process</h2>
+        <ul>
+          <li>Agile/Scrum</li>
+          <li>Specification writing</li>
+          <li>User acceptance testing</li>
+          <li>Go-live and production support</li>
+        </ul>
+        <h1>Work Experience</h1>
+        <h1>Education</h1>
+      </div>
+    </div>
+  </Layout>
+);
+
+export default Home; // eslint-disable-line import/no-default-export
