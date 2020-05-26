fdd72895e6755b28c1f2c107f118aa2d2aef4e8d
Matthew T. Gilbride <mgilbride@chariotsolutions.com>
2020-05-26 19:55:40 -0400

    feat(mgilbride): resume - get the animations working the way I want
    includes some complex hook logic to measure elements but is a nice demo of hooks

--- packages/mgilbride/src/pages/resume/components/Section.tsx
+++ packages/mgilbride/src/pages/resume/components/Section.tsx
@@ -1,5 +1,5 @@
-import React, { FC, useState } from 'react';
-import { animated, useTransition } from 'react-spring';
+import React, { FC, useCallback, useEffect, useState } from 'react';
+import { animated, useSpring } from 'react-spring';
 import { CSSObject } from '@emotion/core';
 import {
   ScrollableSectionHeader,
@@ -7,19 +7,45 @@ import {
 } from './ScrollableSectionHeader';
 
 const styleBody: CSSObject = {
-  transformOrigin: 'top',
+  overflowY: 'hidden',
 };
 
 export const Section: FC<{
   defaultClosed?: boolean;
   headerProps: Omit<ScrollableSectionHeaderProps, 'onClick' | 'open'>;
 }> = ({ defaultClosed, headerProps, children }) => {
-  const [open, setOpen] = useState(!defaultClosed);
+  const [hasRenderedOnce, setHasRenderedOnce] = useState(false);
+  useEffect(() => setHasRenderedOnce(true), []);
 
-  const transitions = useTransition(open, null, {
-    from: { opacity: 0, transform: 'scaleY(0)' },
-    enter: { opacity: 1, transform: 'scaleY(1)' },
-    leave: { opacity: 0, maxHeight: 0, transform: 'scaleY(0)' },
+  const [open, setOpen] = useState(true);
+  if (defaultClosed && !hasRenderedOnce) {
+    // wait a tick to close it so the user sees it can be opened
+    setTimeout(() => setOpen(false), 750);
+  }
+
+  const [bodyHeight, setBodyHeight] = useState(0);
+  const bodyRef = useCallback(
+    (node) => {
+      if (node !== null && open && bodyHeight === 0) {
+        setBodyHeight(node.getBoundingClientRect().height);
+      }
+    },
+    [open, bodyHeight],
+  );
+
+  const bodySpring = useSpring({
+    from: bodyHeight
+      ? {
+          opacity: open ? 0 : 1,
+          maxHeight: open ? 0 : bodyHeight,
+        }
+      : {},
+    to: bodyHeight
+      ? {
+          opacity: open ? 1 : 0,
+          maxHeight: open ? bodyHeight : 0,
+        }
+      : {},
   });
 
   return (
@@ -27,14 +53,9 @@ export const Section: FC<{
       <ScrollableSectionHeader
         {...{ ...headerProps, open, onClick: () => setOpen(!open) }}
       />
-      {transitions.map(
-        ({ item, key, props }) =>
-          item && (
-            <animated.div key={key} style={props} css={styleBody}>
-              {children}
-            </animated.div>
-          ),
-      )}
+      <animated.div ref={bodyRef} style={bodySpring} css={styleBody}>
+        {children}
+      </animated.div>
     </div>
   );
 };
