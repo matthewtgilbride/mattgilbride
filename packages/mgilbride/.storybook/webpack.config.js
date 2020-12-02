1ca535274c77ff43cf7566d736b31675ed31eb92
Matthew T. Gilbride <mgilbride@chariotsolutions.com>
2020-12-02 18:01:28 -0500

    get storybook "working"

---
+++ packages/mgilbride/.storybook/webpack.config.js
@@ -0,0 +1,17 @@
+// https://github.com/duncanleung/gatsby-typescript-emotion-storybook/blob/master/.storybook/webpack.config.js
+module.exports = ({ config }) => {
+  config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    loader: require.resolve('babel-loader'),
+    options: {
+      presets: [
+        // ['react-app', { flow: false, typescript: true }],
+        // Emotion preset must run BEFORE reacts preset to properly convert css-prop.
+        // Babel preset-ordering runs reversed (from last to first). Emotion has to be after React preset.
+        require.resolve('@emotion/babel-preset-css-prop'),
+      ],
+    },
+  });
+
+  return config
+}
